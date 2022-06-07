c45142607fabb90de78af255e768b137f5ee5991
dahye <>
2022-06-07 15:32:30 +0900

    ✅ 드론 업체 등록 테스트 코드 추가

---
+++ src/test/java/com/drop/dropshop/droneCompany/service/DroneCompanyServiceTest.java
@@ -0,0 +1,56 @@
+package com.drop.dropshop.droneCompany.service;
+
+import com.drop.dropshop.droneCompany.dto.DroneCompanyDto;
+import com.drop.dropshop.droneCompany.entity.DroneCompany;
+import com.drop.dropshop.droneCompany.exception.BusinessNumberNotValidException;
+import com.drop.dropshop.droneCompany.repository.DroneCompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DroneCompanyServiceTest {
+    @Autowired
+    private DroneCompanyRepository droneCompanyRepository;
+    @Autowired
+    private DroneCompanyService droneCompanyService;
+
+    @Test
+    void 사업자_번호_검증_성공() {
+        Boolean result = droneCompanyService.validateBuisenessNumber("3098106517");
+        assertEquals(result, true);
+    }
+
+    @Test
+    void 사업자_번호_검증_실패() {
+        Boolean result = droneCompanyService.validateBuisenessNumber("0000000000");
+        assertEquals(result, false);
+    }
+
+    @Test
+    void 드론_업체_등록_실패() throws BusinessNumberNotValidException {
+        DroneCompanyDto droneCompanyDto = new DroneCompanyDto(
+                "11드론",
+                "0313327312",
+                "000000000"
+        );
+        BusinessNumberNotValidException e = assertThrows(BusinessNumberNotValidException.class,
+                () -> droneCompanyService.join(droneCompanyDto));
+        assertThat(e.getMessage()).isEqualTo("사업자 번호 검증 실패");
+    }
+
+    @Test
+    void 드론_업체_등록_성공() throws BusinessNumberNotValidException {
+        DroneCompanyDto droneCompanyDto = new DroneCompanyDto(
+                "11드론",
+                "0313327312",
+                "3098106517"
+        );
+        DroneCompany droneCompany = droneCompanyService.join(droneCompanyDto);
+        assertEquals(droneCompanyDto.getCompanyName(), droneCompany.getCompanyName());
+    }
+}
