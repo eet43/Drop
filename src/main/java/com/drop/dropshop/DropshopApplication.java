eed87d66d95599f9d298414f5f074c861fe82570
dahye <>
2022-06-07 12:28:47 +0900

    📝 Timestamped 추가

--- src/main/java/com/drop/dropshop/DropshopApplication.java
+++ src/main/java/com/drop/dropshop/DropshopApplication.java
@@ -2,7 +2,9 @@ package com.drop.dropshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DropshopApplication {
 
