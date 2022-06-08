da2ebb183986c2ae0903524c5d4b453684027886
dahye <>
2022-06-08 11:58:13 +0900

    🔧 error enum RESOURCE_NOT_FOUND 추가

--- src/main/java/com/drop/dropshop/droneCompany/exception/ErrorCode.java
+++ src/main/java/com/drop/dropshop/droneCompany/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     BUSINESS_NUMBER_NOT_VALID(400, "BUSINESS NUMBER NOT VALID"),
-    INTER_SERVER_ERROR(500,"INTER SERVER ERROR"),;
+    INTER_SERVER_ERROR(500, "INTER SERVER ERROR"),
+    RESOURCE_NOT_FOUND(404, "RESOURCE NOT FOUND");
 
     private int status;
     private String message;
