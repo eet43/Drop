4b3fe719a20af498af87227914a520f3328977c3
hyeoKing <cksgurwkd12@naver.com>
2022-06-09 18:48:23 +0900

    FEAT: 현재 위치 정보를 다룰 Dto 추가

---
+++ src/main/java/com/drop/dropshop/deliveryAdmin/dto/CurrentLocationDto.java
@@ -0,0 +1,15 @@
+package com.drop.dropshop.deliveryAdmin.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CurrentLocationDto {
+    private String currentArea1;
+    private String currentArea2;
+    private String currentArea3;
+}
