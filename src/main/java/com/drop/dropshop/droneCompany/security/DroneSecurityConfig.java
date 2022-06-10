a549bf0903606b0b1e9a77d90c26ea759bea1fcc
dahye <>
2022-06-10 17:02:43 +0900

    🔧 spring security 사용 설정 정렬

--- src/main/java/com/drop/dropshop/droneCompany/security/DroneSecurityConfig.java
+++ src/main/java/com/drop/dropshop/droneCompany/security/DroneSecurityConfig.java
@@ -11,15 +11,23 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class DroneSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtProvider jwtProvider;
+    private final LogoutSuccessHandler logoutSuccessHandler;
 
     @Bean
     public PasswordEncoder encodePassword() {
@@ -35,26 +43,26 @@ public class DroneSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
-            // rest api이므로 csrf 보안이 필요없으므로 disable처리
-            .csrf().disable()
-            // jwt token으로 인증하므로 stateless 하도록 처리
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests()
-            .antMatchers("/api/drone-companies/login").permitAll()
-            .antMatchers("/api/drone-companies/logout").permitAll()
-            .antMatchers(HttpMethod.POST, "/api/drone-companies").permitAll()
-            .antMatchers("/api/drone-companies*").hasRole("ADMIN")
-            .antMatchers("/api/drone-companies*/**").hasRole("ADMIN")
-            .antMatchers("/api/drone-models*").hasRole("ADMIN")
-            .antMatchers("/api/drone-models*/**").hasRole("ADMIN")
-            .antMatchers("/api/drone-companies/**").hasRole("DRONE_COMPANY")
-            .antMatchers("/app/drones").permitAll()
-            .anyRequest().permitAll()
-            .and()
-            .exceptionHandling()
-            .and()
-            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+                // rest api이므로 csrf 보안이 필요없으므로 disable처리
+                .csrf().disable()
+                // jwt token으로 인증하므로 stateless 하도록 처리
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/drone-companies/login").permitAll()
+                .antMatchers("/api/drone-companies/logout").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/drone-companies").permitAll()
+                .antMatchers("/api/drone-companies*").hasRole("ADMIN")
+                .antMatchers("/api/drone-companies*/**").hasRole("ADMIN")
+                .antMatchers("/api/drone-models*").hasRole("ADMIN")
+                .antMatchers("/api/drone-models*/**").hasRole("ADMIN")
+                .antMatchers("/api/drone-companies/**").hasRole("DRONE_COMPANY")
+                .antMatchers("/app/drones").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .exceptionHandling()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
     }
 
 }
