413bd5162d111fae99e46892560fe7dae097aea5
dahye <>
2022-06-13 18:10:01 +0900

    👔 DroneModel findByModelId 추가

--- src/main/java/com/drop/dropshop/droneCompany/repository/DroneModelRepository.java
+++ src/main/java/com/drop/dropshop/droneCompany/repository/DroneModelRepository.java
@@ -4,7 +4,10 @@ import com.drop.dropshop.droneCompany.entity.DroneModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface DroneModelRepository extends JpaRepository<DroneModel, Long> {
     Optional<DroneModel> findByModelName(String modelName);
+
+    Optional<DroneModel> findByModelId(UUID droneModelId);
 }
