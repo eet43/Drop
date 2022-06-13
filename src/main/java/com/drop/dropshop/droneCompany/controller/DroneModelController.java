c8ce0aeb066c5e44490e091c8ac76a624bb4b739
dahye <>
2022-06-13 19:25:28 +0900

    🚚 Drone Model controller 추가

--- src/main/java/com/drop/dropshop/droneCompany/controller/DroneModelController.java
+++ src/main/java/com/drop/dropshop/droneCompany/controller/DroneModelController.java
@@ -7,16 +7,22 @@ import com.drop.dropshop.droneCompany.entity.DroneCompany;
 import com.drop.dropshop.droneCompany.entity.DroneModel;
 import com.drop.dropshop.droneCompany.exception.BusinessNumberNotValidException;
 import com.drop.dropshop.droneCompany.exception.NoResourceException;
+import com.drop.dropshop.droneCompany.response.PagingResponseDetails;
 import com.drop.dropshop.droneCompany.response.ResponseDetails;
 import com.drop.dropshop.droneCompany.service.DroneModelService;
 import com.drop.dropshop.droneCompany.util.HttpStatusChangeInt;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -56,6 +62,22 @@ public class DroneModelController {
         String path = "/api/drone-models/" + id;
         int httpStatus = HttpStatusChangeInt.ChangeStatusCode("OK");
         ResponseDetails responseDetails = new ResponseDetails(new Date(), uuidDto, httpStatus, path);
+        return new ResponseEntity<>(responseDetails, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/drone-models")
+    @ApiOperation(value = "드론 모델 조회", notes = "관리자가 드론 모델을 조회합니다.")
+    public ResponseEntity<?> deleteDroneModel(@PageableDefault(page = 1, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<DroneModel> droneModelList = droneModelService.show(pageable);
+        String path = "/api/drone-models";
+        int httpStatus = HttpStatusChangeInt.ChangeStatusCode("OK");
+        PagingResponseDetails pagingResponseDetails = new PagingResponseDetails(new Date(),
+                droneModelList.getContent(),
+                droneModelList.getTotalElements(),
+                droneModelList.getPageable().getPageNumber() + 1,
+                droneModelList.getPageable().getPageSize(),
+                httpStatus, path);
+        ResponseDetails responseDetails = new ResponseDetails(new Date(), pagingResponseDetails, httpStatus, path);
         return new ResponseEntity<>(responseDetails, HttpStatus.OK);
     }
 }
