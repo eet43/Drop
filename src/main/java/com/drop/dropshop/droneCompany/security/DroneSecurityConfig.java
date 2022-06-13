2b89c38f199d1f1d021f999b574bc642c139e1c0
dahye <>
2022-06-13 14:46:19 +0900

    🔥 불필요한 코드 제거

--- src/main/java/com/drop/dropshop/droneCompany/security/DroneSecurityConfig.java
+++ src/main/java/com/drop/dropshop/droneCompany/security/DroneSecurityConfig.java
@@ -7,27 +7,20 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class DroneSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtProvider jwtProvider;
-    private final LogoutSuccessHandler logoutSuccessHandler;
 
     @Bean
     public PasswordEncoder encodePassword() {
