ed92a928408bd9dc439d6c41a21d0ffcf6974367
hyeoKing <cksgurwkd12@naver.com>
2022-06-15 00:59:20 +0900

    FIX: JpaRepository에 대한 ID 자료형을 Long -> String으로 변경

--- src/main/java/com/drop/dropshop/deliveryAdmin/repository/AnnounceLogRepository.java
+++ src/main/java/com/drop/dropshop/deliveryAdmin/repository/AnnounceLogRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AnnounceLogRepository extends JpaRepository<AnnounceLog, Long> {
+public interface AnnounceLogRepository extends JpaRepository<AnnounceLog, String> {
 }
