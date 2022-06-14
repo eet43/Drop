1d0e392614d6eb0cb96625ed118435ca358179d6
hyeoKing <cksgurwkd12@naver.com>
2022-06-14 22:54:14 +0900

    FEAT: AnnounceLogService 구현 + AnnounceLog 객체를 builder를 통해 생성, + AnnounceLog에 들어가는 값은 requestCreateLogDTO를 통해 들어온 값을 이용 + !! RETURN: 저장에 성공한 객체의 OrderId값을 리턴해줌 + @RequiredArgsConstructor를 통해 의존성 주입 간소화

--- src/main/java/com/drop/dropshop/deliveryAdmin/service/AnnounceLogService.java
+++ src/main/java/com/drop/dropshop/deliveryAdmin/service/AnnounceLogService.java
@@ -1,28 +1,25 @@
 package com.drop.dropshop.deliveryAdmin.service;
 
-import com.drop.dropshop.deliveryAdmin.dto.AnnounceLogDto;
-import com.drop.dropshop.deliveryAdmin.dto.DeliveryStatusDto;
+import com.drop.dropshop.deliveryAdmin.dto.createDTO.RequestCreateLogDTO;
 import com.drop.dropshop.deliveryAdmin.entity.AnnounceLog;
-import com.drop.dropshop.deliveryAdmin.entity.DeliveryStatus;
 import com.drop.dropshop.deliveryAdmin.repository.AnnounceLogRepository;
-import com.drop.dropshop.deliveryAdmin.repository.DeliveryStatusRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AnnounceLogService {
     private final AnnounceLogRepository announceLogRepository;
 
-    @Autowired
-    public AnnounceLogService(AnnounceLogRepository announceLogRepository){ this.announceLogRepository = announceLogRepository;}
-
-    @Transactional
-    public void createAnnounceLog(AnnounceLogDto requestDto){
-        // requestDto에서 current값만 utill의 NaverGeo를 이용해 변환된 값만 넣어줌
-        AnnounceLog announceLog= new AnnounceLog(requestDto);
-        this.announceLogRepository.save(announceLog);
+    public String createAnnounceLog(RequestCreateLogDTO requestCreateLogDTO){
+        AnnounceLog announceLog = AnnounceLog.builder()
+                .orderId(requestCreateLogDTO.getOrderId())
+                .phoneNumber(requestCreateLogDTO.getPhoneNumber())
+                .currentLocation(requestCreateLogDTO.getCurrentLocation())
+                .build();
+        return this.announceLogRepository.save(announceLog).getOrderId();
     }
 }
