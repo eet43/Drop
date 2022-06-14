9db9854d4dad22735d4f35c464b84707d92ddd8c
hyeoKing <cksgurwkd12@naver.com>
2022-06-14 22:42:26 +0900

    FEAT: AnnounceLog 엔티티 생성 + phoneNumber은 당장 사용하지 않는 속성이다 + 배송 위치는 ron, rat 형태로 currentLocation에 저장된다 + 일반적인 모든 조회는 orderId를 통해 조회한다

--- src/main/java/com/drop/dropshop/deliveryAdmin/entity/AnnounceLog.java
+++ src/main/java/com/drop/dropshop/deliveryAdmin/entity/AnnounceLog.java
@@ -1,43 +1,34 @@
 package com.drop.dropshop.deliveryAdmin.entity;
 
-import com.drop.dropshop.deliveryAdmin.dto.AnnounceLogDto;
-import com.drop.dropshop.deliveryAdmin.dto.DeliveryStatusDto;
 import com.drop.dropshop.deliveryAdmin.util.DeliveryLogTimestamped;
-import com.drop.dropshop.deliveryAdmin.util.DeliveryStatusTimestamped;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
-@Setter
-@Getter
+@Setter @Getter
 @NoArgsConstructor
 @Entity
 public class AnnounceLog extends DeliveryLogTimestamped{
     // 배송 상태 엔티티//
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @ApiModelProperty(example = "db id, apply auto_increase: 배달 상태 로그 테이블 아이디")
-    private Long id;
-
     @Column(nullable = false, unique = true, length = 30)
     @ApiModelProperty(example = "orderId, Table identification information: 주문 아이디")
     private String orderId;
 
     @Column(nullable = false, length = 30)
-    @ApiModelProperty(example = "phoneNumber, Identification information for KakaoTalk notification: 카카오톡 알림을 위한 요청자 전화번호")
+    @ApiModelProperty(example = "phoneNumber, Identification information for KakaoTalk notification: 카카오톡 알림, 문자 및 상담을 위한 전화번호")
     private String phoneNumber;
 
-    @Column(nullable = false, length = 150)
-    @ApiModelProperty(example = "currentLocation, Current location of drone in delivery: 배송중인 드론의 현재 위치")
+    @Column(nullable = false, length = 30)
+    @ApiModelProperty(example = "currentLocation, Current location of drone in delivery: 배송중인 드론의 현재 위치(ron, rat)")
     private String currentLocation;
 
-
-    public AnnounceLog(AnnounceLogDto requestDto){
-        this.orderId = requestDto.getOrderId();
-        this.phoneNumber = requestDto.getPhoneNumber();
-        this.currentLocation = requestDto.getCurrentLocation();
+    @Builder
+    public AnnounceLog(String orderId, String phoneNumber, String currentLocation){
+        this.orderId = orderId;
+        this.phoneNumber = phoneNumber;
+        this.currentLocation = currentLocation;
     }
 }
