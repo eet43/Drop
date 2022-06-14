3d461f45295e3ad06385c5741341514b4b146c82
dahye <>
2022-06-14 16:28:28 +0900

    ⚡️ 관리자 기능 식별을 위해 url 내 /admin/ 추가

--- src/main/java/com/drop/dropshop/droneCompany/controller/DroneCompanyController.java
+++ src/main/java/com/drop/dropshop/droneCompany/controller/DroneCompanyController.java
@@ -31,7 +31,7 @@ public class DroneCompanyController {
         this.droneCompanyService = droneCompanyService;
     }
 
-    @PostMapping("/api/drone-companies")
+    @PostMapping("/api/admin/drone-companies")
     @ApiOperation(value = "드론 업체 등록", notes = "관리자가 드론 업체를 등록합니다.")
     public ResponseEntity<?> createDroneCompany(@RequestBody DroneCompanyDto requestDto) throws BusinessNumberNotValidException {
         DroneCompany droneCompany = droneCompanyService.join(requestDto);
@@ -41,7 +41,7 @@ public class DroneCompanyController {
         return new ResponseEntity<>(responseDetails, HttpStatus.CREATED);
     }
 
-    @GetMapping("/api/drone-companies")
+    @GetMapping("/api/admin/drone-companies")
     @ApiOperation(value = "드론 업체 리스트 조회", notes = "관리자가 드론 업체를 조회합니다.")
     public ResponseEntity<?> showAllDroneCompany(@PageableDefault(page = 1, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<DroneCompany> droneCompanyList = droneCompanyService.show(pageable);
@@ -56,7 +56,7 @@ public class DroneCompanyController {
         return new ResponseEntity<>(pagingResponseDetails, HttpStatus.OK);
     }
 
-    @DeleteMapping("/api/drone-companies/{id}")
+    @DeleteMapping("/api/admin/drone-companies/{id}")
     @ApiOperation(value = "드론 업체 삭제", notes = "관리자가 드론 업체를 삭제합니다.")
     public ResponseEntity<?> deleteDroneCompany(@PathVariable UUID id) throws NoResourceException {
         UUID result = droneCompanyService.delete(id);
