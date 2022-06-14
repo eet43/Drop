a74da5fc2aa8e2896e50af2f82d430350c53be9e
hyeoKing <cksgurwkd12@naver.com>
2022-06-14 23:54:04 +0900

    FEAT: AnnounceLogController의 Post 구현 + 택배 배송 요청을 하는 단말 장치로부터 POST 요청을 받는다 + requestCreateLogDTO의 형식으로 body 값을 전달받는다 + announceService의 createAnnounceLog을 이용해 받아온 orderId를 요청자에게 리턴해준다

--- src/main/java/com/drop/dropshop/deliveryAdmin/controller/AnnounceLogController.java
+++ src/main/java/com/drop/dropshop/deliveryAdmin/controller/AnnounceLogController.java
@@ -1,24 +1,24 @@
 package com.drop.dropshop.deliveryAdmin.controller;
 
-import com.drop.dropshop.deliveryAdmin.dto.AnnounceLogDto;
+import com.drop.dropshop.deliveryAdmin.dto.createDTO.RequestCreateLogDTO;
 import com.drop.dropshop.deliveryAdmin.service.AnnounceLogService;
 import com.drop.dropshop.deliveryAdmin.util.ReverseGeocode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RequestMapping("/api/announce")
+@RequiredArgsConstructor
 @RestController
 public class AnnounceLogController {
     private final AnnounceLogService announceLogService;
 
-    @Autowired
-    public AnnounceLogController(AnnounceLogService announceLogService){this.announceLogService = announceLogService;}
-    @PostMapping("/api/announce")
-    public String createAnnounceLog(@RequestBody AnnounceLogDto requestDto){
-        ReverseGeocode reverseGeocode = new ReverseGeocode();
-        announceLogService.createAnnounceLog(requestDto);
-        String response = reverseGeocode.ReverseGeo();
-        return reverseGeocode.fromStringToJson(response).toString();
+    @PostMapping("/log") // 배송요청 단말장치로 부터
+    public String createAnnounceLog(@RequestBody RequestCreateLogDTO requestCreateLogDTO){
+        return announceLogService.createAnnounceLog(requestCreateLogDTO);
     }
 }
