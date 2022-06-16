69777490774a0c3f33530797fe193801d2f7a76b
hyeoKing <cksgurwkd12@naver.com>
2022-06-16 15:35:41 +0900

    FEAT: DeliveryStatusAdminService에 조회를 위한 getDeliveryStatus(String orderId) 추가 + 아이디가 존재하는지 확인하는 예외처리 구현, + Return: build를 통해 ResponseDeliveryStatusDTO 생성 후 리턴

--- src/main/java/com/drop/dropshop/deliveryAdmin/service/DeliveryStatusAdminService.java
+++ src/main/java/com/drop/dropshop/deliveryAdmin/service/DeliveryStatusAdminService.java
@@ -1,6 +1,7 @@
 package com.drop.dropshop.deliveryAdmin.service;
 
 import com.drop.dropshop.deliveryAdmin.dto.createDTO.RequestCreateStatusDTO;
+import com.drop.dropshop.deliveryAdmin.dto.responseDTO.ResponseDeliveryStatusDTO;
 import com.drop.dropshop.deliveryAdmin.entity.DeliveryStatus;
 import com.drop.dropshop.deliveryAdmin.repository.DeliveryStatusRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,5 +18,14 @@ public class DeliveryStatusAdminService {
                 .requestCreateStatusDTO(requestCreateStatusDTO)
                 .build();
         return deliveryStatusRepository.save(deliveryStatus).getOrderId();
+    }
+
+    public ResponseDeliveryStatusDTO getDeliveryStatus(String orderId){
+        DeliveryStatus deliveryStatus = deliveryStatusRepository.findById(orderId).orElseThrow(
+                ()-> new NullPointerException("아이디가 존재하지 않습니다람쥐")
+        );
+        return ResponseDeliveryStatusDTO.builder()
+                .deliveryStatus(deliveryStatus)
+                .build();
     }
 }
