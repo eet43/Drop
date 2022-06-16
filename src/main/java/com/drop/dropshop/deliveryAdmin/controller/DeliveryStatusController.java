6d723497a94e41618ce8f30c6020721271e264f4
hyeoKing <cksgurwkd12@naver.com>
2022-06-16 15:39:57 +0900

    FEAT: DeliveryStatusController의 GET Method 작성, 조회를 위한 ResponseDeliveryStatusDTO를 service로부터 받아 ApiReponse 형태로 리턴해줌

--- src/main/java/com/drop/dropshop/deliveryAdmin/controller/DeliveryStatusController.java
+++ src/main/java/com/drop/dropshop/deliveryAdmin/controller/DeliveryStatusController.java
@@ -2,21 +2,24 @@ package com.drop.dropshop.deliveryAdmin.controller;
 
 import com.drop.dropshop.deliveryAdmin.common.ApiResponse;
 import com.drop.dropshop.deliveryAdmin.dto.createDTO.RequestCreateStatusDTO;
-import com.drop.dropshop.deliveryAdmin.entity.DeliveryStatus;
+import com.drop.dropshop.deliveryAdmin.dto.responseDTO.ResponseDeliveryStatusDTO;
 import com.drop.dropshop.deliveryAdmin.service.DeliveryStatusAdminService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/delivery-status")
 public class DeliveryStatusController {
     private final DeliveryStatusAdminService deliveryStatusAdminService;
 
-    @PostMapping("/api/delivery-status")
+    @PostMapping("/specification")
     public ApiResponse<String> createDeliverySpecification(@RequestBody RequestCreateStatusDTO requestCreateStatusDTO){
         return ApiResponse.success("result", deliveryStatusAdminService.createDeliveryStatus(requestCreateStatusDTO)); // request의 데이터가 삽입된 DeliveryStatus 객체 반환
+    }
+
+    @GetMapping("/specification/{orderId}")
+    public ApiResponse<ResponseDeliveryStatusDTO> getDeliverySpecification(@PathVariable String orderId){
+        return ApiResponse.success("result", deliveryStatusAdminService.getDeliveryStatus(orderId));
     }
 }
