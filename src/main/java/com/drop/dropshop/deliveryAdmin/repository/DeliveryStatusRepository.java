aebaf781db501e434c257126ed94f8e247bdc639
hyeoKing <cksgurwkd12@naver.com>
2022-06-16 15:32:46 +0900

    FIX: 기존 잘못 정의된 ID의 자료형을 Long->String으로 변경

--- src/main/java/com/drop/dropshop/deliveryAdmin/repository/DeliveryStatusRepository.java
+++ src/main/java/com/drop/dropshop/deliveryAdmin/repository/DeliveryStatusRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface DeliveryStatusRepository extends JpaRepository<DeliveryStatus, Long> {
+public interface DeliveryStatusRepository extends JpaRepository<DeliveryStatus, String> {
 
 }
